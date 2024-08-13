221f237e0b10300db49d9c626ba48827a2776d14
SergWebAcceptance <serg.web@acceptance.ltd>
2024-08-13 17:02:19 +0300

    Added preloader

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Urbanist } from "next/font/google";
 import "@/styles/base.scss";
 import Footer from "@/global_components/Footer";
 import Header from "@/global_components/Header";
+import Preloader from "@/global_components/Preloader";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={urbanist.className}>
+        <Preloader />
         <Header />
         <main>{children}</main>
         <Footer />
