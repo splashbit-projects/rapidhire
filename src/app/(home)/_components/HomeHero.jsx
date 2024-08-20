96b53f14a991517dffd797231e68be8f0481fd81
SergWebAcceptance <serg.web@acceptance.ltd>
2024-08-20 10:29:27 +0300

    Fixed images quality

--- src/app/(home)/_components/HomeHero.jsx
+++ src/app/(home)/_components/HomeHero.jsx
@@ -63,7 +63,7 @@ const HomeHero = () => {
             </motion.div>
           </div>
         </div>
-        <Image src="/images/home/homeHero.png" width={1110} height={400} />
+        <Image src="/images/home/homeHero.png" quality={100} width={1110} height={400} />
       </div>
     </section>
   );
