814c559676d9e18f88e574d01ccf512767bf27e9
SergWebAcceptance <serg.web@acceptance.ltd>
2024-08-20 16:24:55 +0300

    changed logos

--- src/app/(home)/_components/HomeTools.jsx
+++ src/app/(home)/_components/HomeTools.jsx
@@ -25,20 +25,9 @@ const HomeTools = () => {
             viewport={{ once: true }}
             variants={fadeInUp}
           >
-            <h3>Workday Analytics</h3>
+            <h3>PayScale</h3>
             <div>
-              <img src="/images/home/logos/logo1.png" />
-            </div>
-          </motion.div>
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            variants={fadeInUp}
-          >
-            <h3>Greenhouse</h3>
-            <div>
-              <img src="/images/home/logos/logo2.png" />
+              <img src="/images/home/logos/logo1.svg" />
             </div>
           </motion.div>
           <motion.div
@@ -49,7 +38,18 @@ const HomeTools = () => {
           >
             <h3>Glint</h3>
             <div>
-              <img src="/images/home/logos/logo3.png" />
+              <img src="/images/home/logos/logo2.svg" />
+            </div>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInUp}
+          >
+            <h3>Greenhouse</h3>
+            <div>
+              <img src="/images/home/logos/logo3.svg" />
             </div>
           </motion.div>
           <motion.div
@@ -60,7 +60,7 @@ const HomeTools = () => {
           >
             <h3>Lattice</h3>
             <div>
-              <img src="/images/home/logos/logo4.png" />
+              <img src="/images/home/logos/logo4.svg" />
             </div>
           </motion.div>
           <motion.div
@@ -71,40 +71,7 @@ const HomeTools = () => {
           >
             <h3>ComplyRight</h3>
             <div>
-              <img src="/images/home/logos/logo5.png" />
-            </div>
-          </motion.div>
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            variants={fadeInUp}
-          >
-            <h3>PayScale</h3>
-            <div>
-              <img src="/images/home/logos/logo6.png" />
-            </div>
-          </motion.div>
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            variants={fadeInUp}
-          >
-            <h3>Cornerstone OnDemand</h3>
-            <div>
-              <img src="/images/home/logos/logo7.png" />
-            </div>
-          </motion.div>
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            variants={fadeInUp}
-          >
-            <h3>BambooHR</h3>
-            <div>
-              <img src="/images/home/logos/logo8.png" />
+              <img src="/images/home/logos/logo5.svg" />
             </div>
           </motion.div>
           <motion.div
@@ -115,7 +82,40 @@ const HomeTools = () => {
           >
             <h3>SAP SuccessFactors</h3>
             <div>
-              <img src="/images/home/logos/logo9.png" />
+              <img src="/images/home/logos/logo6.svg" />
+            </div>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInUp}
+          >
+            <h3>Cornerstone OnDemand</h3>
+            <div>
+              <img src="/images/home/logos/logo7.svg" />
+            </div>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInUp}
+          >
+            <h3>Workday Analytics</h3>
+            <div>
+              <img src="/images/home/logos/logo8.svg" />
+            </div>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInUp}
+          >
+            <h3>BambooHR</h3>
+            <div>
+              <img src="/images/home/logos/logo9.svg" />
             </div>
           </motion.div>
           <motion.div
@@ -126,7 +126,7 @@ const HomeTools = () => {
           >
             <h3>ADP Workforce Now</h3>
             <div>
-              <img src="/images/home/logos/logo10.png" />
+              <img src="/images/home/logos/logo10.svg" />
             </div>
           </motion.div>
         </div>
